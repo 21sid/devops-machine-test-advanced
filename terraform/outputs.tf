d604699e663a0b630e4820b1285b564a748570c4
21sid <141542830+21sid@users.noreply.github.com>
2026-05-02 14:40:06 +0530

    Define Terraform outputs for Azure resources
    Added multiple outputs for Azure resources including AKS, ACR, Key Vault, MySQL, and Log Analytics.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,57 @@
 # Define all required Terraform outputs here (AKS cluster name, ACR login server, Key Vault URI, etc.)
+output "resource_group_name" {
+  description = "Name of the created resource group"
+  value       = azurerm_resource_group.rg.name
+}
+
+output "aks_cluster_name" {
+  description = "Name of the AKS cluster"
+  value       = azurerm_kubernetes_cluster.aks.name
+}
+
+output "aks_cluster_endpoint" {
+  description = "AKS cluster API server endpoint"
+  value       = azurerm_kubernetes_cluster.aks.kube_config[0].host
+  sensitive   = true
+}
+
+output "acr_login_server" {
+  description = "ACR login server URL"
+  value       = azurerm_container_registry.acr.login_server
+}
+
+output "acr_name" {
+  description = "Name of the Azure Container Registry"
+  value       = azurerm_container_registry.acr.name
+}
+
+output "key_vault_uri" {
+  description = "URI of the Azure Key Vault"
+  value       = azurerm_key_vault.kv.vault_uri
+}
+
+output "key_vault_name" {
+  description = "Name of the Azure Key Vault"
+  value       = azurerm_key_vault.kv.name
+}
+
+output "mysql_server_fqdn" {
+  description = "FQDN of the MySQL Flexible Server"
+  value       = azurerm_mysql_flexible_server.mysql.fqdn
+}
+
+output "managed_identity_client_id" {
+  description = "Client ID of the User Assigned Managed Identity"
+  value       = azurerm_user_assigned_identity.aks_identity.client_id
+}
+
+output "log_analytics_workspace_id" {
+  description = "ID of the Log Analytics Workspace"
+  value       = azurerm_log_analytics_workspace.law.id
+}
+
+output "kubeconfig" {
+  description = "Raw kubeconfig for AKS cluster"
+  value       = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive   = true
+}
