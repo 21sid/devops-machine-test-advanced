25749b799bb17067da99d412f527c44175eb14e9
21sid <141542830+21sid@users.noreply.github.com>
2026-05-02 14:39:34 +0530

    Add Terraform configuration for Azure resources
    This commit adds a comprehensive Terraform configuration for provisioning Azure resources including resource groups, virtual networks, AKS, ACR, MySQL servers, and Key Vaults. It also includes necessary configurations for networking, logging, and access policies.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,234 @@
 # Provision all Azure resources here: Resource Group, VNet, AKS, ACR, MySQL Flexible Server, Key Vault, Managed Identity, Log Analytics.
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = var.vnet_address_space
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.tags
+}
+
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "subnet-aks"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "mysql_subnet" {
+  name                 = "subnet-mysql"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+
+  delegation {
+    name = "mysql-delegation"
+    service_delegation {
+      name = "Microsoft.DBforMySQL/flexibleServers"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action"
+      ]
+    }
+  }
+}
+
+resource "azurerm_log_analytics_workspace" "law" {
+  name                = var.log_analytics_workspace_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+  tags                = var.tags
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Standard"
+  admin_enabled       = false
+  tags                = var.tags
+}
+
+resource "azurerm_user_assigned_identity" "aks_identity" {
+  name                = "identity-aks-microservices"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.tags
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.aks_cluster_name
+
+  # ✅ FIXED VERSION
+  kubernetes_version = "1.33"
+
+  default_node_pool {
+    name                = "default"
+    node_count          = var.aks_node_count
+    vm_size             = var.aks_node_vm_size
+    vnet_subnet_id      = azurerm_subnet.aks_subnet.id
+    enable_auto_scaling = true
+    min_count           = 2
+    max_count           = 10
+  }
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.aks_identity.id]
+  }
+
+  # ✅ FIXED NETWORK CONFIG
+  network_profile {
+    network_plugin    = "azure"
+    network_policy    = "calico"
+    load_balancer_sku = "standard"
+    service_cidr      = "172.16.0.0/16"
+    dns_service_ip    = "172.16.0.10"
+  }
+
+  oms_agent {
+    log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+  }
+
+  key_vault_secrets_provider {
+    secret_rotation_enabled = true
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                       = var.key_vault_name
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  tenant_id                  = data.azurerm_client_config.current.tenant_id
+  sku_name                   = "standard"
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = true
+  tags                       = var.tags
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    secret_permissions = ["Get", "List", "Set", "Delete", "Recover"]
+  }
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = azurerm_user_assigned_identity.aks_identity.principal_id
+    secret_permissions = ["Get", "List"]
+  }
+}
+
+resource "azurerm_private_dns_zone" "mysql_dns" {
+  name                = "microservices.mysql.database.azure.com"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "mysql_dns_link" {
+  name                  = "mysql-dns-vnet-link"
+  private_dns_zone_name = azurerm_private_dns_zone.mysql_dns.name
+  resource_group_name   = azurerm_resource_group.rg.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+}
+
+resource "azurerm_mysql_flexible_server" "mysql" {
+  # ✅ FIXED UNIQUE NAME
+  name                   = var.mysql_server_name
+  resource_group_name    = azurerm_resource_group.rg.name
+  location               = azurerm_resource_group.rg.location
+  administrator_login    = var.mysql_admin_username
+  administrator_password = var.mysql_admin_password
+  sku_name               = "B_Standard_B1ms"
+  version                = "8.0.21"
+  delegated_subnet_id    = azurerm_subnet.mysql_subnet.id
+  private_dns_zone_id    = azurerm_private_dns_zone.mysql_dns.id
+  zone                   = "1"
+
+  storage {
+    size_gb = 20
+  }
+
+  high_availability {
+    mode = "Disabled"
+  }
+
+  tags = var.tags
+
+  depends_on = [azurerm_private_dns_zone_virtual_network_link.mysql_dns_link]
+}
+
+resource "azurerm_mysql_flexible_database" "db" {
+  name                = var.mysql_db_name
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_mysql_flexible_server.mysql.name
+  charset             = "utf8mb4"
+  collation           = "utf8mb4_unicode_ci"
+}
+
+resource "azurerm_key_vault_secret" "db_host" {
+  name         = "db-host"
+  value        = azurerm_mysql_flexible_server.mysql.fqdn
+  key_vault_id = azurerm_key_vault.kv.id
+}
+
+resource "azurerm_key_vault_secret" "db_user" {
+  name         = "db-user"
+  value        = var.mysql_admin_username
+  key_vault_id = azurerm_key_vault.kv.id
+}
+
+resource "azurerm_key_vault_secret" "db_password" {
+  name         = "db-password"
+  value        = var.mysql_admin_password
+  key_vault_id = azurerm_key_vault.kv.id
+}
+
+resource "azurerm_key_vault_secret" "db_name" {
+  name         = "db-name"
+  value        = var.mysql_db_name
+  key_vault_id = azurerm_key_vault.kv.id
+}
+
+resource "azurerm_key_vault_secret" "jwt_secret" {
+  name         = "jwt-secret"
+  value        = "CHANGE_ME_STRONG_JWT_SECRET_KEY_32CHARS"
+  key_vault_id = azurerm_key_vault.kv.id
+}
+
+resource "azurerm_monitor_diagnostic_setting" "aks_diagnostics" {
+  name                       = "aks-diagnostics"
+  target_resource_id         = azurerm_kubernetes_cluster.aks.id
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+
+  enabled_log {
+    category = "kube-apiserver"
+  }
+
+  enabled_log {
+    category = "kube-controller-manager"
+  }
+
+  metric {
+    category = "AllMetrics"
+    enabled  = true
+  }
+}
